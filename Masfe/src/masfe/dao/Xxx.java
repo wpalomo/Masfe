bfb7ebc8555f0f862a401967eb6faa38794978e0
jimmy22 <jimmy.vasquez22@gmail.com>
2014-07-04 00:32:30 -0500

    prueba de uso doble

--- Masfe/src/masfe/dao/Xxx.java
+++ Masfe/src/masfe/dao/Xxx.java
@@ -1,5 +1,37 @@
 package masfe.dao;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Xxx {
 
+	 public static String getRutaFirma() {
+	        String rutaFirma;
+	        Properties prop = new Properties();
+	        try {
+	            prop.load(new FileInputStream("firma.properties"));
+	            rutaFirma = prop.getProperty("ruta_firma");
+	        } catch (IOException ex) {
+	            rutaFirma = null;
+	        }
+
+	        return rutaFirma;
+
+	    }
+
+	 public static String getRutaFirma2() {
+	        String rutaFirma;
+	        Properties prop = new Properties();
+	        try {
+	            prop.load(new FileInputStream("firma.properties"));
+	            rutaFirma = prop.getProperty("ruta_firma");
+	        } catch (IOException ex) {
+	            rutaFirma = null;
+	        }
+
+	        return rutaFirma;
+
+	    }
+	
 }
