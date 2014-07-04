566843984cd4dd92a90c50b6c2424b0cc830aa27
jimmy22 <jimmy.vasquez22@gmail.com>
2014-07-04 00:14:14 -0500

    estructura de carpetas anteriores

---
+++ Masfe/src/masfe/Masfe.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package masfe;
+
+/**
+ * Clase principal del proyecto.
+ * Punto de entrada al sistema.
+ * @author josue
+ */
+public class Masfe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hola Safem");
+    }
+    
+}
